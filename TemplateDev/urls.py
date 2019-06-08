5a15041461085fa56ab63d830f14602cf4b9d37b
SatanChrist <prs.garg@gmail.com>
2019-06-08 12:26:50 +0530

    change url

--- TemplateDev/urls.py
+++ TemplateDev/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/', include('home.urls')),
+    url(r'^', include('home.urls')),
     ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
